b59333d39ba5a71a6c751f28bf3fcf2b58aa1d52
nickssun <nmalchev@gmail.com>
2024-02-01 12:53:07 +0200

    Add TODO steps for refactoring

--- src/App/Handler/PermissionHandler.php
+++ src/App/Handler/PermissionHandler.php
@@ -12,6 +12,8 @@ use ProgPhil1337\SimpleReactApp\HTTP\Routing\Handler\HandlerInterface;
 use ProgPhil1337\SimpleReactApp\HTTP\Routing\HttpMethod;
 use ProgPhil1337\SimpleReactApp\HTTP\Routing\RouteParameters;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\HttpFoundation\Response;
+
 
 #[Route(httpMethod: HttpMethod::GET, uri: '/has_permission/{token}')]
 class PermissionHandler implements HandlerInterface
@@ -25,35 +27,49 @@ class PermissionHandler implements HandlerInterface
 
     public function __invoke(ServerRequestInterface $serverRequest, RouteParameters $parameters): ResponseInterface
     {
+        //TODO what is NP?
+        //TODO add constants
         $np = "read";
 
+        //GET requested Token
         $tId = $parameters->get("token", "kein_token");
 
+
+        //TODO reverse the exception check
         if ($tId != "kein_token") {
+
             $dataProvider = new TokenDataProvider();
 
             $tokens = $dataProvider->getTokens();
             $token = null;
 
             foreach ($tokens as $t) {
+                //TODO add strict checks
                 if ($t["token"] == $tId) {
                     $token = $t;
                 }
             }
 
             foreach ($token["permissions"] as $p) {
+                //TODO add strict checks, descriptive properties names
                 if ($p == $np) {
+                    //TODO initialize $a
                     $a = $a + 1;
                 }
             }
 
+            //TODO $a could be undefined here
             if ($a > 0) {
-                return new JSONResponse(array("permission" => true), 400);
+
+                //TODO add common client response, set proper HTTP status codes, list permissions
+                $permission = [];
+                return new JSONResponse($permission, Response::HTTP_OK);
             } else {
-                return new JSONResponse(array('permission' => false), 400);
+                //TODO -
+                return new JSONResponse([], Response::HTTP_UNAUTHORIZED);
             }
         } else {
-            return new JSONResponse(array("permission" => false), 400);
+            return new JSONResponse([], Response::HTTP_BAD_REQUEST);
         }
     }
 }
